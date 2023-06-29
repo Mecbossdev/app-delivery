c595970624abb6a231261bd95a430fc5bc2df831
Mecbossdev <fernandesjeferson539@gmail.com>
2023-06-29 20:18:22 -0300

    create init page product

---
+++ src/app/product/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import styled from 'styled-components'
+
+interface ProductProps {}
+
+const Container = styled.div``
+
+export default function Product(props: ProductProps) {
+  return (
+    <Container>
+      <h1>Produto</h1>
+    </Container>
+  )
+}
